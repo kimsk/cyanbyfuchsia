ce7a87f91401f5448ebd1dd2a5ee1a28e56ed6a0
kimsk112 <karlkim@outlook.com>
2013-10-28 23:17:56 -0500

    Debugging Design-Time WinRT XAML Binding in Blend
    Code for
    http://cyanbyfuchsia.wordpress.com/2013/10/28/debugging-design-time-winrt-xaml-binding-in-blend/

---
+++ DebugXamlBinding/DebugXamlBinding/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugXamlBinding
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            Title = "Hello Runtime!";            
+        }
+        public string Title { get; set; }
+    }
+}
